d50abcb6f521d849760da96843ff5d8480223bad
Cora Gelin <coragelin@mac.students.netlogon.liu.se>
2026-02-16 13:50:51 +0100

    button visable on site

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,10 @@ import Search from './components/SearchButton'
 function App() {
   return (
     <div className="App">
-      <h1>GR8</h1>
+      <h1 className="text-3xl font-bold text-center text-gray-800">GR8</h1>
       <Search />
     </div>
    )
 }
 
 export default App
-
-
-
--- frontend/src/components/Searchbutton.jsx
+++ frontend/src/components/Searchbutton.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Search() {
   const [clicked, setClicked] = useState(false);

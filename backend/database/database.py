3752d6d744048d5482243ced2367262b321cb2f4
Einar Cederlund <einarxcederlund@gmail.com>
2026-02-24 10:35:17 +0100

    final database file

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,8 +1,36 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import base64
+from pathlib import Path
 import sqlite3
 from datetime import datetime
 from pathlib import Path
 
 DB_PATH = Path(__file__).with_name("analysis.sqlite")
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/image/{name}")
+def get_image(name: str):
+    if name == "bov":
+        image_path = Path(__file__).with_name(f"output.jpg")
+        image_bytes = image_path.read_bytes()
+        image_b64 = base64.b64encode(image_bytes).decode("utf-8")
+
+        return {
+            "name": name,
+            "image": image_b64,
+        }
+    else:
+        raise HTTPException(status_code=404, detail="Image not found")
 
 
 def create_database() -> None:
